11617f0ebae397e4eca93401ffe90358a930a557
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-03 14:32:37 -0600

    fourth

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,1 +1,7 @@
-const products = require('../products.json')
+const products = require('../products.json');
+
+const getProducts = (req, res) => {
+    res.status(200).send(products);
+}
+
+module.exports = getProducts;
