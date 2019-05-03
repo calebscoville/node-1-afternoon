44f9ace2d89ef561972737cb814128a63de800b8
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-03 16:01:50 -0600

    Close

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const products = require('../products.json');
+const getProducts = require('./getProducts');
 
 const app = express();
 
-const port = 3002;
+const port = 3001;
 
-app.get('/api/products', (req, res) => {
-    res.status(200).send(products)
-});
+app.get('/api/products', getProducts);
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
-})
+});
